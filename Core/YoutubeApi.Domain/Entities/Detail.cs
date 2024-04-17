69aabad4d88e6874e3d5993d962bf8a98b414aa8
begumeygun <begumeygun@gmail.com>
2024-04-17 14:26:53 +0300

    ders5

--- Core/YoutubeApi.Domain/Entities/Detail.cs
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
 
 namespace YoutubeApi.Domain.Entities
 {
-    public class Detail
+    public class Detail : EntityBase
     {
         public Detail()
         {
